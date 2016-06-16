57f99408deae698deed009f8d87811c1ecbb5b7e
admin2 <admin2@134-68-178-229.dhcp-in.iupui.edu>
2016-06-16 11:34:42 -0400

    Added credit thing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
+
+
+  get 'about', to: 'pages#credit'
+
+  get 'pages/credit'
+  get 'credit', to: 'pages#credit'
+
+
+
   resources :transcriptions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'transcriptions#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
